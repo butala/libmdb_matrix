d2d07f32bbd41b5763f700e91ff440d8a3c8ad4a
butala <mark.d.butala@gmail.com>
2019-04-25 15:25:22 +0800

    Fixed typo.

--- python/setup.py
+++ python/setup.py
@@ -4,4 +4,4 @@ from setuptools import setup
 setup(name='libmdbmatrix',
       vesrion='0.1',
       author='Mark D. Butala',
-      py_modules=['libmdbmatrix'])
+      py_modules=['libmdb_matrix'])
